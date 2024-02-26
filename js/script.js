0ce6b47a21519b8d497a3ed84dc3d606939f977c
luisustain <160989518+luisustain@users.noreply.github.com>
2024-02-26 12:33:39 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,16 +6,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // List of quadrants and their enlarged images
   const quadrants = [
-    { id: 'A1', imageUrl: 'images/A1.jpeg' },
-    { id: 'A2', imageUrl: 'images/A2.jpeg' },
-    { id: 'B1', imageUrl: 'images/B1.jpeg' },
-    { id: 'B2', imageUrl: 'images/B2.jpeg' },
+    { id: 'A1', imageUrl: 'images/A1.jpg' },
+    { id: 'A2', imageUrl: 'images/A2.jpg' },
+    { id: 'B1', imageUrl: 'images/B1.jpg' },
+    { id: 'B2', imageUrl: 'images/B2.jpg' },
   ];
 
   // Set event listeners for each quadrant
   quadrants.forEach(quadrant => {
     const element = document.getElementById(quadrant.id);
     element.addEventListener('mouseover', () => setEnlargedBackground(quadrant.id, quadrant.imageUrl));
-    element.addEventListener('mouseout', () => setEnlargedBackground(quadrant.id, 'images/background.jpeg')); // Reset to the initial large image
+    element.addEventListener('mouseout', () => setEnlargedBackground(quadrant.id, 'images/background.jpg')); // Reset to the initial large image
   });
 });
